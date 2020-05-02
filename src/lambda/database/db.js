ae40b17c0f22b0a15370d23238a44bc00b0bc207
Lukasz_Kochajewski <lkochajewski@pgs-soft.com>
2020-05-02 17:49:43 +0200

    add mongodb

--- src/lambda/database/db.js
+++ src/lambda/database/db.js
@@ -1,16 +1,26 @@
-// server.js
-import mongoose from 'mongoose'
-require('dotenv').config()
-// Initialize connection to database
-const dbUrl = process.env.DB_URI
-const dbOptions = {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true
-}
-// Set DB from mongoose connection
-mongoose.connect(dbUrl, dbOptions)
-const db = mongoose.connection
-// eslint-disable-next-line no-console
-db.on('error', console.error.bind(console, 'MongoDB connection error:'))
-export default db
+// // server.js
+// import mongoose from 'mongoose'
+// require('dotenv').config()
+// // Initialize connection to database
+// const dbUrl = process.env.DB_URI
+// const dbOptions = {
+//   useNewUrlParser: true,
+//   useFindAndModify: false,
+//   useUnifiedTopology: true
+// }
+// // Set DB from mongoose connection
+// mongoose.connect(dbUrl, dbOptions)
+// const db = mongoose.connection
+// // eslint-disable-next-line no-console
+// db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+// export default db
+
+import { MongoClient } from 'mongodb'
+import dotenv from 'dotenv'
+dotenv.config()
+
+MongoClient.connect(process.env.DB_URI, function (err, db) {
+  if (err) { throw err }
+  console.log('Database created!')
+  db.close()
+})
--- src/lambda/recipes.js
+++ src/lambda/recipes.js
@@ -1,22 +1,50 @@
 
-import './database/db'
-import Recipe from './database/recipeModel'
+// import './database/db'
+// import Recipe from './database/recipeModel'
 
-exports.handler = async (event, context) => {
+import { MongoClient } from 'mongodb'
+import dotenv from 'dotenv'
+dotenv.config()
+
+exports.handler = (event, context) => {
   context.callbackWaitsForEmptyEventLoop = false
-  try {
-    const data = await Recipe.find({}).exec()
-    return {
-      statusCode: 200,
-      body: JSON.stringify(data),
-      headers: {
-        'content-type': 'application/json',
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Headers':
+
+  MongoClient.connect(process.env.DB_URI, function (err, connection) {
+    if (err) { throw err }
+    // eslint-disable-next-line no-console
+    console.log('Database created!')
+    const db = connection.db('test')
+    const recipeCollections = db.collection('recipes')
+    recipeCollections.find({}).toArray(function (err, result) {
+      if (err) { throw err }
+      db.close()
+      return {
+        statusCode: 200,
+        body: JSON.stringify(result),
+        headers: {
+          'content-type': 'application/json',
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Headers':
         'Origin, X-Requested-With, Content-Type, Accept'
+        }
       }
-    }
-  } catch (err) {
-    return { statusCode: 500, body: err.toString() }
-  }
+    })
+  })
+
+  // try {
+  //   // const data = await Recipe.find({}).exec()
+
+  //   return {
+  //     statusCode: 200,
+  //     body: JSON.stringify(data),
+  //     headers: {
+  //       'content-type': 'application/json',
+  //       'Access-Control-Allow-Origin': '*',
+  //       'Access-Control-Allow-Headers':
+  //       'Origin, X-Requested-With, Content-Type, Accept'
+  //     }
+  //   }
+  // } catch (err) {
+  //   return { statusCode: 500, body: err.toString() }
+  // }
 }
